1b38e28bf6d5ce6ce24c3821437e289b916cf49d
yedim <yelimi0416@gmail.com>
2017-12-10 17:00:46 +0900

    RePermutation(중복순열)

---
+++ DataStructure/Graph/10-4 RePerMutation.c
@@ -0,0 +1,38 @@
+//중복순열
+//n개의 데이터 중 r개를 중복을 허용하여 배열하는 경우. 주사위 던지기
+#include <stdio.h>
+#define R 3 
+//주사위 갯수
+int b[R] = { 0, };
+int cnt = 0;
+
+void PrintRePer(){
+	int i;
+
+	cnt++;
+	for (i = 0; i < R; i++)
+	{
+		printf("%3d", b[i]);
+	}
+	printf("\n");
+}
+void RePermutation(int a[], int n, int r){
+	int i;
+	if (r == R) PrintRePer();
+	else
+		for (i = 0; i < n; i++){
+			b[r] = a[i];
+			RePermutation(a, n, r + 1);
+		}
+
+}
+int main(void){
+	int a[] = { 1, 2, 3, 4, 5, 6 };
+	int n = sizeof(a) / sizeof(int);
+
+
+	printf("%d개 주사위의 중복 순열\n", R);
+	RePermutation(a, n, 0);
+	printf("중복순열 개수 : %d\n", cnt);
+	return 0;
+}
